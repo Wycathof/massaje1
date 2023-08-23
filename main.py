934c5312ff07cef7d1852c4032586e1e8b0db59b
ArVip <gleb000@yandex.ru>
2023-08-23 18:17:00 +0400

    callback all

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ clients = {}
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Чтобы записаться на маникюр, выбери дату и время',
-                     reply_markup=create_calendar())
+    bot.send_message(message.chat.id, 'Привет! Чтобы записаться на маникюр, выбери дату и время', reply_markup=create_calendar())
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('select_date'))
 def callback_date_selected(call):
     date = call.data.split('|')[1]
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                          text=f'Выбрана дата {date}. Теперь выбери время', reply_markup=create_time_selection(date))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f'Выбрана дата {date}. Теперь выбери время', reply_markup=create_time_selection(date))
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('back_to_date'))
+def callback_back_to_date(call):
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Привет! Чтобы записаться на маникюр, выбери дату и время',
+                          reply_markup=create_calendar())
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('select_time'))
@@ -40,6 +44,12 @@ def callback_time_selected(call):
                                   reply_markup=create_time_selection(date))
 
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('back_to_time'))
+def callback_back_to_time(call):
+    date = call.data.split('|')[1]
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f'Выбрана дата {date}. Теперь выбери время',
+                          reply_markup=create_time_selection(date))
+
 @bot.message_handler(commands=['schedule'])
 def send_schedule(message):
     schedule_text = 'Моё расписание на следующий месяц:\n'
@@ -47,7 +57,6 @@ def send_schedule(message):
         schedule_text += date + '\n'
         schedule_text += '\n'.join(times) + '\n\n'
     bot.send_message(message.chat.id, schedule_text)
-
 
 @bot.message_handler(commands=['cancel'])
 def cancel_message(message):
@@ -58,10 +67,9 @@ def cancel_message(message):
         bot.send_message(message.chat.id, f'Отменено. Время {time} ({date}) освободилось')
         notify_admin_cancelled(message.chat.id, date, time)
 
-
 @bot.message_handler(commands=['clients'])
 def send_clients_list(message):
-    if message.from_user.id == ADMIN_CHAT_ID:
+    if message.from_user.id == 6119033891:
         clients_list = 'Список записавшихся клиентов:\n'
         for chat_id, data in clients.items():
             clients_list += f'{chat_id}: {data["date"]} {data["time"]}\n'
@@ -77,37 +85,33 @@ def create_calendar():
         calendar.add(button)
     return calendar
 
-
 def create_time_selection(date):
     times = schedule[date]
     time_selection = types.InlineKeyboardMarkup()
     for time in times:
         button = types.InlineKeyboardButton(text=time, callback_data=f'select_time|{date}|{time}')
         time_selection.add(button)
+    back_button = types.InlineKeyboardButton(text="Назад", callback_data="back_to_date")
+    time_selection.add(back_button)
     return time_selection
-
 
 def is_available(date, time):
     return time in schedule[date]
 
-
 def remove_time_from_schedule(date, time):
     schedule[date].remove(time)
 
-
 def notify_admin(user_id, date, time):
-    message = bot.send_message(ADMIN_CHAT_ID,
-                               f'Новый клиент записался на маникюр: {user_id}, {date} {time}. Подтвердить запись?',
+    message = bot.send_message(ADMIN_CHAT_ID, f'Новый клиент записался на маникюр: {user_id}, {date} {time}. Подтвердить запись?',
                                reply_markup=create_confirmation_keyboard(user_id))
-
 
 def create_confirmation_keyboard(user_id):
     confirmation_keyboard = types.InlineKeyboardMarkup()
     confirm_button = types.InlineKeyboardButton(text='Подтвердить', callback_data=f'confirm|{user_id}')
     cancel_button = types.InlineKeyboardButton(text='Отменить', callback_data=f'cancel|{user_id}')
-    confirmation_keyboard.add(confirm_button, cancel_button)
+    back_button = types.InlineKeyboardButton(text="Назад", callback_data=f'back_to_clients|{user_id}')
+    confirmation_keyboard.add(confirm_button, cancel_button, back_button)
     return confirmation_keyboard
-
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('confirm'))
 def callback_confirm_selected(call):
@@ -119,7 +123,6 @@ def callback_confirm_selected(call):
 
     bot.answer_callback_query(call.id)
 
-
 @bot.callback_query_handler(func=lambda call: call.data.startswith('cancel'))
 def callback_cancel_selected(call):
     user_id = int(call.data.split('|')[1])
@@ -127,15 +130,18 @@ def callback_cancel_selected(call):
         date, time = clients[user_id]['date'], clients[user_id]['time']
         schedule[date].append(time)
         del clients[user_id]
-        bot.send_message(user_id, 'Ваша запись на маникюр была отменена. Вы можете записаться на другое время.')
+        bot.send_message(user_id, f'Ваша запись на маникюр была отменена. Вы можете записаться на другое время.')
     else:
         bot.send_message(user_id, 'К сожалению, ваша запись уже была отменена или не существует.')
 
     bot.answer_callback_query(call.id)
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('back_to_clients'))
+def callback_back_to_clients(call):
+    user_id = int(call.data.split('|')[1])
+    bot.answer_callback_query(call.id)
 
 def notify_admin_cancelled(user_id, date, time):
     bot.send_message(ADMIN_CHAT_ID, f'Клиент отменил запись на маникюр: {user_id}, {date} {time}')
-
 
 bot.polling()
